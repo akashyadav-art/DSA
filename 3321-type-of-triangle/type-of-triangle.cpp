5049e78770181951bcbb1be6e77b9defd67151eb
Akash kumar gope <akashkumargope681@gmail.com>
2025-07-28 08:23:39 +0530

    Time: 0 ms (100.00%) | Memory: 22.8 MB (87.85%) - LeetSync

--- 3321-type-of-triangle/type-of-triangle.cpp
+++ 3321-type-of-triangle/type-of-triangle.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
-    string triangleType(vector<int>& nums) {
-        int n=nums.size();
-   set<int >s;
+    string triangleType(vector<int>& nums)
+   {
    sort(nums.begin(),nums.end());
    if(nums[0]+nums[1]<=nums[2]) return "none";
  if (nums[0]==nums[2]) return "equilateral";
