d8854171eb6edacc1ce0b7ee25fdb0a0f08fbbeb
Akash kumar gope <akashkumargope681@gmail.com>
2025-08-02 20:56:08 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (64.16%) - LeetSync

--- 3321-type-of-triangle/type-of-triangle.cpp
+++ 3321-type-of-triangle/type-of-triangle.cpp
@@ -6,7 +6,7 @@ public:
    if(nums[0]+nums[1]<=nums[2]) return "none";
  if (nums[0]==nums[2]) return "equilateral";
     if (nums[0]==nums[1] || nums[1]==nums[2]) return "isosceles";
-    return "scalene";
+      else return "scalene";
     }
 };
    
