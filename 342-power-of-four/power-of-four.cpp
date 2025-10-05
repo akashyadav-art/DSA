4a68140b81c1445ac18615816242bee47a23f048
Akash kumar gope <akashkumargope681@gmail.com>
2025-10-06 02:31:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (5.88%) - LeetSync

---
+++ 342-power-of-four/power-of-four.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+          if(n==1) return true;
+          if(n<=0) return false ;
+          double x= log(n)/log(4);
+          return round(x)==x;
+          
+    }
+};
