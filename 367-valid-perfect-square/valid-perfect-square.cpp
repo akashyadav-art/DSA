bccb27dd2b0f1f578e7f7dd0f6b8d23d0cfaca2e
Akash kumar gope <akashkumargope681@gmail.com>
2026-08-23 13:04:53 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (18.48%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.cpp
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-         for(long i=1;i*i<=num;i++)
-         
+         for(long i=1;i*i<=num;i++)  // use long for i*i is overflow.         
             if( i*i==num)
             {
                 return true;
