6de12f17d786168aeecd87f8301e21c8a7c233c5
Akash kumar gope <akashkumargope681@gmail.com>
2026-08-23 09:54:45 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (73.00%) - LeetSync

--- 476-number-complement/number-complement.cpp
+++ 476-number-complement/number-complement.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    int findComplement(int num) {
-        unsigned mask=~0;
-        while(mask & num) mask =mask<<1;
-        return ~num ^ mask;
+    int findComplement(int num) {  // num=5
+        unsigned mask=~0; //~num=010
+        while(mask & num) mask =mask<<1; //mask=11111000
+        return ~num ^ mask; // 010 ^11111000=2
     }
 };
