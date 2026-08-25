9dcee6867177667bed20804557219c6812b418bf
Akash kumar gope <akashkumargope681@gmail.com>
2026-08-25 21:59:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (79.77%) - LeetSync

--- 258-add-digits/add-digits.cpp
+++ 258-add-digits/add-digits.cpp
@@ -1,8 +1,16 @@
 class Solution {
 public:
     int addDigits(int num) {
-        if (num==0)  return 0;
-        if (num%9==0)  return 9;
-        else return num%9; 
-    }
+         int sum=0;
+        while(num>=10)
+         { sum=0;
+        while(num>0)
+        {int temp=num%10;
+            sum+=temp;
+            num/=10;
+        }
+     num=sum;
+        }   
+        return num;      
+       }
 };
