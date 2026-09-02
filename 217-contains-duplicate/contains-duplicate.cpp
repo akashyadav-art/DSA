bb342851d7fe3dab76492bd473e7d4a6137409a9
Akash kumar gope <akashkumargope681@gmail.com>
2026-09-02 06:57:22 +0530

    Time: 119 ms (10.84%) | Memory: 116.3 MB (5.04%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int>freq;
-        
-            for(int i=0;i<nums.size();i++)
-            {
-                freq[nums[i]]++;
-            }
-        
-        for(int i=0;i<nums.size();i++){
-        if(freq[nums[i]]>1)
-         return true;
+        set<int>s(nums.begin(),nums.end());
+        if(nums.size()!=s.size())
+        {
+            return true;
         }
         return false;
-    
+        
     }
-    
 };
